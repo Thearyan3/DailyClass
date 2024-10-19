ca4729f148c1392298de8d5e1292ace1a92b6deb
Aryan <chaudharyaryan3456@gmail.com>
2024-10-19 21:44:11 +0530

    Added day

---
+++ Day3/index.js
@@ -0,0 +1,11 @@
+// Arithmetic Operators (%, +, -, *, /)
+// let a = 5;
+// let b =  10;
+// let res = a % b;
+// console.log(res);
+
+// Comparison Operatos (==, ===, !=, !==, <=, >=, <, >)
+let c = "10";
+let d = 10;
+let resu = c <= d;
+console.log(resu);
