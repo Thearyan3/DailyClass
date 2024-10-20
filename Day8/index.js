ddfa5324de5eb3beb5561dba8569a7f2211b6325
Aryan <chaudharyaryan3456@gmail.com>
2024-10-20 23:22:34 +0530

    updated

---
+++ Day8/index.js
@@ -0,0 +1,46 @@
+// (viii). reverse() method - The reverse() method 
+// const
+
+
+// Question - You are provided an array of integer and you have to print the sum of array. 
+// let arr = [1, 2, 3, 4];
+// let sum = 0;
+// for(let i = 0; i < arr.length; i++){
+//     sum += arr[i]; // or sum = sum + arr[i];
+// }
+// console.log(sum);
+
+// (viii). Reverse() and sort() method 
+// const arr = [1, 2, 3, 4, 5, 6, 2, 3, 7, 93, -12];
+// console.log(arr.reverse());
+// console.log(arr.sort());
+
+// (ix). includes() method 
+// const arr = [1, 2, 3, 4, 5];
+// console.log(arr.includes(5));
+
+// (x). splice() and slice() method 
+// const arr = [1, 2, 3, 4, 5];
+// console.log(arr.splice(0, 1, 10));
+// console.log(arr);
+// console.log(arr.splice(1, 3, 'a', 'b', 'c', 'd'));
+// console.log(arr);
+// console.log(arr.slice(1, 3));
+// console.log(arr);
+
+// (xi). unshift() method 
+// const arr1 = [1, 2, 3, 4, 5];
+// console.log(arr1.unshift(10));
+// console.log(arr1);
+
+// (xii). concat() and join() methods 
+// let myArr = [1, 2, 3];
+// console.log(myArr.concat(5, 6, 7));
+
+// let MyArr = ["Hi", "How", "Are", "You"];
+// console.log(MyArr.join("_"));
+
+// (xiii). indexOf() and lastIndexOf() method 
+let myArr = [1, 2, 3, 4, 5, 6, 3];
+console.log(myArr.indexOf(3));
+console.log(myArr.lastIndexOf(3));
