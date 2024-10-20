63b4e137905e22cee4945af4aa484558c74b73bd
Aryan <chaudharyaryan3456@gmail.com>
2024-10-20 21:31:43 +0530

    added Day6

---
+++ Day6/index.js
@@ -0,0 +1,64 @@
+// Question - Given an array , find out the total no. of odd and even nos. 
+// let arr = [1, 2, 5, 7, 8, 3, 9, 1, 3, 4, 5];
+// let totalEven = 0;
+// let totalOdd = 0;
+// for(let i = 1; i <= arr.length; i++){
+//     if(arr[i] % 2 == 0){
+//         totalEven++;
+//     }else{
+//         totalOdd++;
+//     }
+// }
+// console.log("Total no. of even Numbers are :", totalEven);
+// console.log("Total no. of Odd Numbers are :", totalOdd);
+
+// (iv). While loop 
+// let i = 1;
+// while(i < 10){
+//     text = "The number is : " + i;
+//     i++;
+//     console.log(text);
+// }
+
+// (v). do-while loop
+// let i = 1;
+// do{
+//     console.log("Hello Aryan");
+//     i++;
+// }while(i < 14);
+
+// 17. Strings 
+// var name = "You will be an amazing developer";
+// let index = name.indexOf("developer");
+// console.log(index);
+// index = name.lastIndexOf("developer");
+// console.log(index);
+// index = name.indexOf("developer", 60);
+// console.log(index);
+
+// * Methods of String
+// (i). String concatenation
+// var String1 = "Prep";
+// var string2 = "Bytes";
+// var string3 = String1 + string2;
+// console.log(string3);
+
+// (ii). Concat() Method - The concat() method is used to join two or more strings. 
+// var str1 = "Hello";
+// var str2 = "Maria!";
+// var res = str1.concat(str2);
+// console.log(res);
+
+// (iii). slice() Method 
+// var name = "You will be an amazing developer";
+// var newMessage = name.slice(7, 13);
+// console.log(newMessage);
+// newMessage = name.slice(7, -13);
+// console.log(newMessage);
+// newMessage = name.slice(7);
+// console.log(newMessage);
+// newMessage = name.slice(-7);
+// console.log(newMessage);
+
+// (iv).
+
