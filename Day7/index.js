a20f46d022cef1c20e0098e9ee6bef9d2ea239c9
Aryan <chaudharyaryan3456@gmail.com>
2024-10-20 22:00:42 +0530

    added

--- Day7/index.js
+++ Day7/index.js
@@ -1,5 +1,21 @@
-// (v). Replace() method 
-var name = "You will be an amazing developer";
-console.log(name.replace(' ', '_'));
-console.log(name.replaceAll(' ', '_'));
-console.log(name.trim());
+// (v). Replace() method - The Jacascript replace() changes the defined value to an another value .
+// var name = "You will be an amazing developer";
+// console.log(name.replace(' ', '_'));
+// console.log(name.replaceAll(' ', '_'));
+// console.log(name.trim());
+
+// (vi). Split() Method - The split() method converts strings into arrays.
+// var num = "You will be an amazing developer";
+// var num1 = num.split(' ');
+// console.log(num1);
+
+// (vii). length() Method - It is used to measure the length of a String. 
+// var num = "You will be an amazing developer";
+// console.log(num.length);
+
+// Question - You are provided with a string, You have to slice the name from the string. 
+// let str = "Hi Prepbytes";
+// let sliced = str.slice(3);
+// console.log(sliced);
+
+// 18. Array 
