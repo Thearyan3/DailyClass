98009b730399556584cbc3c68c0e455c1a975320
Aryan <chaudharyaryan3456@gmail.com>
2024-10-24 13:25:58 +0530

    updated

---
+++ Day10/index.js
@@ -0,0 +1,18 @@
+// function a(){
+//     console.log("a");
+// }
+
+const c = () => {
+    console.log("hello Aryan", c);
+}
+c();
+
+const b = function a(){
+    console.log("a");
+}
+b();
+
+function showMessage() {
+    alert("Hello World");
+}
+showMessage();
