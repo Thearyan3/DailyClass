ea6aa2cfb28121f9f4b693c7e8226e8cf773a27f
Aryan <chaudharyaryan3456@gmail.com>
2024-11-06 13:34:20 +0530

    updated

--- Day11/index.js
+++ Day11/index.js
@@ -1,12 +1,19 @@
-function change(){
-let ele = document.getElementsByTagName('p');
- ele[0].innerHTML = "Hello India!";
- ele[1].innerHTML = "Hello India!";
- ele[0].style = "background-color : red";
- let change = document.getElementById("id");
- change.innerHTML = "Id is for div";
- let change2 = document.getElementsByClassName("div");
- change2[0].innerHTML = "class is for div1";
+// function change(){
+// let ele = document.getElementsByTagName('p');
+//  ele[0].innerHTML = "Hello India!";
+//  ele[1].innerHTML = "Hello India!";
+//  ele[0].style = "background-color : red";
+//  let change = document.getElementById("id");
+//  change.innerHTML = "Id is for div";
+//  let change2 = document.getElementsByClassName("div");
+//  change2[0].innerHTML = "class is for div1";
+// }
+
+function card() {
+    let horizontal = document.getElementById("div1");  
+    horizontal.style.display = "flex";                 
+    horizontal.style.flexDirection = "row";          
 }
+
 
 
