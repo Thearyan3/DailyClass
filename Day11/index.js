e474805bc47dcf078b39259c3628c03068f28677
Aryan <chaudharyaryan3456@gmail.com>
2024-11-06 13:10:54 +0530

    updated

--- Day11/index.js
+++ Day11/index.js
@@ -2,4 +2,11 @@ function change(){
 let ele = document.getElementsByTagName('p');
  ele[0].innerHTML = "Hello India!";
  ele[1].innerHTML = "Hello India!";
+ ele[0].style = "background-color : red";
+ let change = document.getElementById("id");
+ change.innerHTML = "Id is for div";
+ let change2 = document.getElementsByClassName("div");
+ change2[0].innerHTML = "class is for div1";
 }
+
+
