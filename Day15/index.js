f858923b4b650d35bda2e2200a5ef347957b2d8a
Aryan <chaudharyaryan3456@gmail.com>
2024-11-14 13:17:30 +0530

    updated

--- Day15/index.js
+++ Day15/index.js
@@ -1,4 +1,4 @@
 function change(){
     let picture = document.getElementsByTagName("img");
-    picture[0].src = 'url("./aryan.jpg")';
+    picture[0].src = "https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcQGHBdKFHpGoXWbE8rxbrtYxJUunCpYfWv3fnt4DYTkHzbqKRnV_Zbe_0r4arjqUw5MnpVrHW8THE5Dm-0";
 }
