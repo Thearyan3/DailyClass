c7fdf573fa4cc3c8b9eaac01ef24682137e1ac8c
Aryan <chaudharyaryan3456@gmail.com>
2024-11-21 13:06:58 +0530

    updated

--- Day17/index.js
+++ Day17/index.js
@@ -7,27 +7,36 @@
 // const rectangle = outer(5);
 // rectangle(8);
 
-const count = () => {
-    setTimeout(() => {
-        console.log("1");
-        setTimeout(() => {
-            console.log("2");
-            setTimeout(() => {
-                console.log("3");
-                setTimeout(() => {
-                    console.log("4");
-                    setTimeout(() => {
-                        console.log("5");
-                        setTimeout(() => {
-                            console.log("6");
-                            setTimeout(() => {
-                                console.log("7")
-                            }, 7000);
-                        }, 6000);
-                    }, 5000);
-                }, 4000);
-            }, 3000);
-        }, 2000);
-    }, 1000);
+// const count = () => {
+//     setTimeout(() => {
+//         console.log("1");
+//         setTimeout(() => {
+//             console.log("2");
+//             setTimeout(() => {
+//                 console.log("3");
+//                 setTimeout(() => {
+//                     console.log("4");
+//                     setTimeout(() => {
+//                         console.log("5");
+//                         setTimeout(() => {
+//                             console.log("6");
+//                             setTimeout(() => {
+//                                 console.log("7")
+//                             }, 7000);
+//                         }, 6000);
+//                     }, 5000);
+//                 }, 4000);
+//             }, 3000);
+//         }, 2000);
+//     }, 1000);
+// };
+// count();
+
+const book = {
+    title : "Javascript Topics",
+    author : "Aryan", 
+    year : 2024,
 };
-count();
+console.log(Object.keys(book));
+console.log(Object.entries(book));
+console.log(Object.values(book));
