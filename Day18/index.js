3e4d023f77a17555820002434157a83a6f880df5
Aryan <chaudharyaryan3456@gmail.com>
2024-11-29 22:03:50 +0530

    updated

--- Day18/index.js
+++ Day18/index.js
@@ -25,3 +25,37 @@
 // console.log(location);
 
 // 3. Destructing with the default values 
+// const person2 = {
+//     name : "Keshav", 
+//     age : 20,
+// }
+
+// const {name, age, city = "Dwarka"} = person2;
+// console.log(name);
+// console.log(age);
+// console.log(city);
+
+// 4.Destructing Nested Objects 
+// const person3 = {
+//     name : "Prashant", 
+//     age : 20, 
+//     address : {
+//         city : "Meerut",
+//         ZipCode : 250001,
+//     }
+// }
+
+// const {name, age, address: {city, ZipCode}} = person3;
+// console.log(name);
+// console.log(age);
+// console.log(city);
+// console.log(ZipCode);
+
+
+// 5. Destructing with the Array
+const numbers = [1, 2, 3, 4];
+const [first, second, third, fourth] = numbers;
+console.log(first);
+console.log(second);
+console.log(third);
+console.log(fourth);
