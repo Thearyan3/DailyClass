c54e90175060fbb727eb44bafc8e3c2678de9efa
Aryan <chaudharyaryan3456@gmail.com>
2024-11-29 21:44:16 +0530

    added Day18

---
+++ Day18/index.js
@@ -0,0 +1,27 @@
+// Object Destructing 
+
+// 1. Basic Object destructing 
+// const person = {
+//     name : "Aryan", 
+//     age : 20, 
+//     city : "Noida"
+// }
+
+// const {name, age, city} = person;
+// console.log(name);
+// console.log(age);
+// console.log(city);
+
+// 2. Destructing with the Variables
+// const person1 = {
+//      name : "Navya",
+//      age : 20,
+//      city : "Meerut"
+// }
+
+// const {name : FullName, age: YearsOld, city : location} = person1;
+// console.log(FullName);
+// console.log(YearsOld);
+// console.log(location);
+
+// 3. Destructing with the default values 
