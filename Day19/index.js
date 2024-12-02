e6ee756bceebf4e42f91471b494ef0470f7dc25a
Aryan <chaudharyaryan3456@gmail.com>
2024-12-02 22:02:36 +0530

    updated

--- Day19/index.js
+++ Day19/index.js
@@ -3,41 +3,41 @@
 // Object = An object is an instance of a class and can hold the data(properties and methods).
 
 // Classes 
-// class Car{
-//     constructor(make, model){
-//         this.make = make;
-//         this.model = model;
-//     }
-//     displayInfo() {
-//         console.log(`Car : ${this.make} ${this.model}`);
-//     }
-// }
+class Car{
+    constructor(make, model){
+        this.make = make;
+        this.model = model;
+    }
+    displayInfo() {
+        console.log(`Car : ${this.make} ${this.model}`);
+    }
+}
 
-// const MyCar = new Car("Toyota", "Corolla");
-// MyCar.displayInfo();
+const MyCar = new Car("Toyota", "Corolla");
+MyCar.displayInfo();
 
 
 // 2. Encapsulation - It refers to binding the data(properties) and methods that operate on the data within one unit(object).
 
-// class BankAccount{
-//     #balance;
-//     constructor(owner, initialBalance){
-//         this.owner = owner;
-//         this.#balance = initialBalance;
-//     }
-//     deposit(amount){
-//         if(amount > 0){
-//         this.#balance += amount;
-//     }
-//     }
-//     getBalance(){
-//         return this.#balance;
-//     }
-// }
+class BankAccount{
+    #balance;
+    constructor(owner, initialBalance){
+        this.owner = owner;
+        this.#balance = initialBalance;
+    }
+    deposit(amount){
+        if(amount > 0){
+        this.#balance += amount;
+    }
+    }
+    getBalance(){
+        return this.#balance;
+    }
+}
 
-// const myAccount = new BankAccount("Aryan", 20000);
-// myAccount.deposit(200);
-// console.log(myAccount);
+const myAccount = new BankAccount("Aryan", 20000);
+myAccount.deposit(200);
+console.log(myAccount);
 
 
 //Inheritance = It allows one class to inherit properties and methods from the another class.
